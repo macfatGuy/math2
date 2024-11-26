13fbc91d013faeec1d41b35209bce33a11b12be4
Влад <@DESKTOP-KIIK14P>
2024-11-26 23:25:16 +0300

    Добавьте файлы проекта.

---
+++ math2/counter.h
@@ -0,0 +1,17 @@
+#ifndef COUNTER_H
+#define COUNTER_H
+
+class Counter {
+private:
+	int value;
+
+public:
+	Counter();
+	Counter(int initialValue);
+	void increment();
+	void decrement();
+	int getValue() const;
+};
+
+#endif 
+
---
+++ math2/math2.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <string>
+#include "counter.h"
+
+Counter::Counter() : value(1) {}
+
+Counter::Counter(int initialValue) : value(initialValue) {}
+
+void Counter::increment() {
+    value++;
+}
+
+void Counter::decrement() {
+    value--;
+}
+
+int Counter::getValue() const {
+    return value;
+}
+
+
+
+using namespace std;
+
+int main() {
+    Counter* counter;
+    string input;
+    setlocale(LC_ALL, "Russian");
+
+
+    cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    cin >> input;
+
+    if (input == "да") {
+        int initialValue;
+        cout << "Введите начальное значение счётчика: ";
+        cin >> initialValue;
+        counter = new Counter(initialValue);
+    }
+    else {
+        counter = new Counter();
+    }
+
+    char command;
+    while (true) {
+        cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        cin >> command;
+
+        switch (command) {
+        case '+':
+            counter->increment();
+            break;
+        case '-':
+            counter->decrement();
+            break;
+        case '=':
+            cout << counter->getValue() << endl;
+            break;
+        case 'x':
+            cout << "До свидания!" << endl;
+            delete counter;
+            return 0;
+        default:
+            cout << "Неверная команда!" << endl;
+            break;
+        }
+    }
+
+    delete counter;
+    return 0;
+}
+
